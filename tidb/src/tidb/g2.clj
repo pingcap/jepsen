48e8334800c92df4709b70ee6f684f91893c3ed7
UncP <uncp.xu@gmail.com>
2017-08-03 16:06:27 +0800

    minor fix
    Signed-off-by: UncP <uncp.xu@gmail.com>

--- tidb/src/tidb/g2.clj
+++ tidb/src/tidb/g2.clj
@@ -10,7 +10,6 @@
             [jepsen.checker.timeline :as timeline]
             [tidb.sql :refer :all]
             [tidb.basic :as basic]
-            [clojure.pprint :refer [pprint]]
             [clojure.java.jdbc :as j]
             [clojure.core.reducers :as r]
             [clojure.set :as set]
@@ -22,45 +21,35 @@
   client/Client
   (setup! [this test node]
     (j/with-db-connection [c (conn-spec (first (:nodes test)))]
-      (j/execute! c "create table a (
-                     id    int primary key,
-                     key   int,
-                     value int)")
-      (j/execute! c "create table b (
-                    id    int primary key,
-                    key   int,
-                    value int)"))
+      (j/execute! c ["create table if not exists a (id int primary key, kk int, value int)"])
+      (j/execute! c ["create table if not exists b (id int primary key, kk int, value int)"]))
 
       (assoc this :node node))
 
   (invoke! [this test op]
-    (let [[k [a-id b-id]] (:value op)]
-      (case (:f op)
-        :insert
-          (with-txn op [c node]
+    (with-txn op [c node]
+      (let [[k [a-id b-id]] (:value op)]
+        (case (:f op)
+          :insert
             (letr [order (< (rand) 0.5)
-              as (j/query c [(str "select * from " (if order "a" "b")
-                            " where key = ? and value % 3 = 0")
+              as (j/query c [(str "select * from " (if order "a" "b") " where kk = ? and value % 3 = 0")
                             k])
-              bs (j/query c [(str "select * from " (if order "b" "a")
-                            " where key = ? and value % 3 = 0")
+              bs (j/query c [(str "select * from " (if order "b" "a") " where kk = ? and value % 3 = 0")
                             k])
               _ (when (or (seq as) (seq bs))
                   ; Ah, the other txn has already committed
                   (return (assoc op :type :fail :error :too-late)))
               table (if a-id "a" "b")
               id    (or a-id b-id)
-              r (j/insert! c table {:key k, :id id, :value 30})]
-            (assoc op :type :ok)))
+              r (j/insert! c :table {:id id, :kk k, :value 30})]
+            (assoc op :type :ok))
 
-        :read
-          (let [as (with-txn-retries (j/query c ["select * from a where
-                                key = ? and value % 3 = 0" k]))
-                bs (with-txn-retries (j/query c ["select * from b where
-                                key = ? and value % 3 = 0" k]))
-                values (->> (concat as bs)
-                            (map :id))]
-                (assoc op :type :ok :value (independent/tuple k values)))))))
+          :read
+            (let [as (j/query c ["select * from a where kk = ? and value % 3 = 0" k])
+                  bs (j/query c ["select * from b where kk = ? and value % 3 = 0" k])
+                  values (->> (concat as bs)
+                              (map :id))]
+                  (assoc op :type :ok :value (independent/tuple k values)))))))
 
   (teardown! [this test]))
 
