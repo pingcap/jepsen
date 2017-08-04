0b7b95060cba703aa68d431bc0c4e0a78b0918f4
UncP <uncp.xu@gmail.com>
2017-08-04 17:17:15 +0800

    minor fix
    Signed-off-by: UncP <uncp.xu@gmail.com>

--- tidb/run.sh
+++ tidb/run.sh
@@ -6,11 +6,11 @@ for test in "bank" "sets" "register"
 do
     for nemesis in "none" "parts" "majority-ring" "start-stop-2" "start-kill-2"
         do
-	    echo lein run test --test ${test} --nemesis ${nemesis} --time-limit 60 --recovery-time 30 --concurrency 10
-        if [ $? -ne 1 ]
-        then
-            echo ${test} ${nemesis} >> failed.log
-        fi
+	        lein run test --test ${test} --nemesis ${nemesis} --time-limit 60 --recovery-time 30 --concurrency 10
+            if [ $? -ne 0 ]
+            then
+                echo ${test} ${nemesis} >> failed.log
+            fi
     done
 done
 
