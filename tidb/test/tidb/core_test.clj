ef911c2aadef41171d9ec66c9b7168b8cecf1712
UncP <uncp.xu@gmail.com>
2017-08-04 10:37:37 +0800

    minor fix
    Signed-off-by: UncP <uncp.xu@gmail.com>

--- tidb/test/tidb/core_test.clj
+++
@@ -1,6 +0,0 @@
-(ns tidb.core-test
-  (:require [bankojure.test :refer :all]
-            [bankojure.string :as string]
-            [jepsen.core :as jepsen]
-            [jepsen.control :as control]
-            ))
