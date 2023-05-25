3e11573d32746db34c3e1979ac92fde69b70eb77
removedporn <86824510+removedporn@users.noreply.github.com>
2023-05-25 09:59:54 +0800

    Delete jepsen directory

--- jepsen/src/jepsen/repl.clj
+++
@@ -1,9 +0,0 @@
-(ns jepsen.repl
-  "Helper functions for mucking around with tests!"
-  (:require [jepsen.store :as store]
-            [jepsen.report :as report]))
-
-(defn latest-test
-  "Returns the most recently run test"
-  []
-  (store/latest))
